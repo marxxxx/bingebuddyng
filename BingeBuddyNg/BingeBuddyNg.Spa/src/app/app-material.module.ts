47414f6350f230bbb57d37e3d3958bfdb6fe87a9
Markus Strobl <markus.strobl@dataformers.at>
2019-03-02 17:31:54 +0100

    updated packages

--- BingeBuddyNg/BingeBuddyNg.Spa/src/app/app-material.module.ts
+++ BingeBuddyNg/BingeBuddyNg.Spa/src/app/app-material.module.ts
@@ -2,12 +2,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule, MatCheckboxModule, MatCardModule, MatGridListModule, MatTabsModule, MatBadgeModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatCardModule, MatTabsModule, MatBadgeModule } from '@angular/material';
 import { MatDialogModule, MatInputModule, MatListModule } from '@angular/material';
 import { MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule } from '@angular/material';
 import { MatSelectModule, MatSidenavModule, MatSlideToggleModule, MatSnackBarModule } from '@angular/material';
 import { MatToolbarModule, MatTooltipModule, MatIconModule } from '@angular/material';
 import { MatChipsModule } from '@angular/material';
+
+
 
 
 @NgModule({
