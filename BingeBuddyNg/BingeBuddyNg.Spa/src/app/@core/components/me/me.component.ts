4fc9de4c46e822d400763c21e22b7964e48c6276
Markus S <marxx@live.at>
2020-11-28 14:33:17 +0100

    added direct update of alc level in toolbar

--- BingeBuddyNg/BingeBuddyNg.Spa/src/app/@core/components/me/me.component.ts
+++ BingeBuddyNg/BingeBuddyNg.Spa/src/app/@core/components/me/me.component.ts
@@ -1,28 +1,36 @@
 import { UserInfoDTO } from '../../../../models/UserInfoDTO';
 import { AuthService } from '../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { merge, Observable } from 'rxjs';
 import { map, filter, tap } from 'rxjs/operators';
 import { UserStatisticsDTO } from 'src/models/UserStatisticsDTO';
+import { StateService } from '../../services/state.service';
 
 @Component({
   selector: 'app-me',
   templateUrl: './me.component.html',
-  styleUrls: ['./me.component.css']
+  styleUrls: ['./me.component.css'],
 })
 export class MeComponent implements OnInit {
-
   userInfo$: Observable<UserInfoDTO>;
-  stats: UserStatisticsDTO;
+  stats$: Observable<UserStatisticsDTO>;
+  currentUserId: string;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private stateService: StateService) {}
 
   ngOnInit() {
     this.userInfo$ = this.authService.currentUserProfile$
-      .pipe(filter(profile => (profile != null)))
-      .pipe(
-        tap(profile => this.stats = profile.user.currentStats),
-        map(profile => ({ userId: profile.sub, userName: profile.nickname }))
-        );
+      .pipe(filter((profile) => profile != null))
+      .pipe(tap(profile => this.currentUserId = profile.sub))
+      .pipe(map((profile) => ({ userId: profile.sub, userName: profile.nickname })));
+
+    this.stats$ = merge(
+      this.authService.currentUserProfile$
+        .pipe(filter((profile) => profile != null))
+        .pipe(map((profile) => profile.user.currentStats)),
+      this.stateService.activityReceived$
+        .pipe(filter((a) => a?.userStats?.userId === this.currentUserId))
+        .pipe(map((a) => a.userStats))
+    );
   }
 }
